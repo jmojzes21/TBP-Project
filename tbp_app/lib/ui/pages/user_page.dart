97f5bec1d2b0931a8adf273a4549c6c28551ec3e
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-31 19:53:55 +0100

    Update user page and logout

--- tbp_app/lib/ui/pages/user_page.dart
+++ tbp_app/lib/ui/pages/user_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tbp_app/models/user.dart';
+import 'package:tbp_app/ui/pages/login_page.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({super.key});
@@ -8,8 +10,34 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  User user = User.current!;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('UserPage'));
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('${user.firstName} ${user.lastName}', style: Theme.of(context).textTheme.titleLarge),
+          const SizedBox(height: 10),
+          Text('Email', style: Theme.of(context).textTheme.titleMedium),
+          Text(user.email, style: Theme.of(context).textTheme.bodyMedium),
+          const SizedBox(height: 10),
+          Text('Kontakt', style: Theme.of(context).textTheme.titleMedium),
+          Text(user.contact, style: Theme.of(context).textTheme.bodyMedium),
+          const SizedBox(height: 20),
+          FilledButton(
+            onPressed: () {
+              User.current = null;
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
+                return const LoginPage();
+              }));
+            },
+            child: const Text('Odjavi se'),
+          ),
+        ],
+      ),
+    );
   }
 }
