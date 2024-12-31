e7d136584bd502c3d7594f4b7b5448e5f8c69038
jmojzes21 <jmojzes21@student.foi.hr>
2024-12-31 18:11:06 +0100

    Create user page

--- tbp_app/lib/ui/app_navigation.dart
+++ tbp_app/lib/ui/app_navigation.dart
@@ -4,6 +4,7 @@ import 'package:tbp_app/ui/pages/activities_page.dart';
 import 'package:tbp_app/ui/pages/eating_overview_page.dart';
 import 'package:tbp_app/ui/pages/physical_indicators_page.dart';
 import 'package:tbp_app/ui/pages/training_planning_page.dart';
+import 'package:tbp_app/ui/pages/user_page.dart';
 
 class AppNavigation extends StatefulWidget {
   const AppNavigation({super.key});
@@ -18,6 +19,7 @@ class _AppNavigationState extends State<AppNavigation> {
     EatingOverviewPage(),
     PhysicalIndicatorsPage(),
     TrainingPlanningPage(),
+    UserPage(),
   ];
 
   int currentPageIndex = 0;
@@ -47,6 +49,10 @@ class _AppNavigationState extends State<AppNavigation> {
           NavigationDestination(
             icon: FaIcon(FontAwesomeIcons.solidChartBar),
             label: 'Planovi',
+          ),
+          NavigationDestination(
+            icon: FaIcon(FontAwesomeIcons.solidUser),
+            label: 'Profil',
           ),
         ],
       ),
