ad5a2516194653fc6d9ac9f257952a1dec138975
jmojzes21 <jmojzes21@student.foi.hr>
2025-01-05 11:11:17 +0100

    Show user activities, add activity

---
+++ tbp_app/lib/logic/activities_service.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:tbp_app/logic/database_connection.dart';
+import 'package:tbp_app/models/activity.dart';
+import 'package:tbp_app/models/activity_type.dart';
+
+class ActivitiesService {
+  Future<List<ActivityType>> getAllActivityTypes() async {
+    var db = DatabaseConnection();
+    await db.open();
+
+    var results = await db.execute('SELECT * FROM "activityType"');
+
+    await db.close();
+
+    return results.map((e) {
+      return ActivityType(
+        key: e['key'],
+        name: e['name'],
+      );
+    }).toList();
+  }
+
+  Future<List<Activity>> getAll(int userId) async {
+    var db = DatabaseConnection();
+    await db.open();
+
+    var results = await db.execute(
+      'SELECT * FROM "activities" WHERE "userId" = @userid',
+      {'userid': userId},
+    );
+
+    await db.close();
+
+    return results.map((e) {
+      return Activity.fromJson(
+        id: e['id'],
+        userId: e['userId'],
+        data: e['data'],
+      );
+    }).toList();
+  }
+
+  Future<void> insert(Activity activity) async {
+    var db = DatabaseConnection();
+    await db.open();
+
+    var sql = 'INSERT INTO "activities" ("userId", "data")';
+    sql += " VALUES (@userId, @data)";
+
+    await db.execute(sql, {
+      'userId': activity.userId,
+      'data': jsonEncode(activity.toJson()),
+    });
+
+    await db.close();
+  }
+}
--- tbp_app/lib/ui/pages/activities_page.dart
+++ tbp_app/lib/ui/pages/activities_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tbp_app/logic/activities_service.dart';
+import 'package:tbp_app/logic/date_formats.dart';
+import 'package:tbp_app/models/activity.dart';
+import 'package:tbp_app/models/activity_type.dart';
+import 'package:tbp_app/models/exceptions.dart';
+import 'package:tbp_app/models/user.dart';
+import 'package:tbp_app/ui/date_time_picker.dart';
+import 'package:tbp_app/ui/dialogs.dart';
 
 class ActivitiesPage extends StatefulWidget {
   const ActivitiesPage({super.key});
@@ -8,8 +16,161 @@ class ActivitiesPage extends StatefulWidget {
 }
 
 class _ActivitiesPageState extends State<ActivitiesPage> {
+  var user = User.current!;
+
+  List<ActivityType> activityTypes = [];
+
+  var tcName = TextEditingController();
+  var tcNotes = TextEditingController();
+
+  ActivityType? activityType;
+  DateTime? startDateTime;
+  DateTime? endDateTime;
+
+  List<Activity> data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  void load() async {
+    var service = ActivitiesService();
+    var activityTypes = await service.getAllActivityTypes();
+    var data = await service.getAll(user.id);
+
+    setState(() {
+      this.activityTypes = activityTypes;
+      this.data = data;
+    });
+  }
+
+  void add() async {
+    var service = ActivitiesService();
+    try {
+      await service.insert(Activity(
+        id: 0,
+        userId: user.id,
+        name: tcName.text.trim(),
+        type: activityType!.key,
+        startDateTime: startDateTime!,
+        endDateTime: endDateTime!,
+        notes: tcNotes.text.trim(),
+      ));
+      Dialogs.showShortSnackBar(this, 'Uspješno');
+      load();
+    } catch (a) {
+      var e = AppException.from(a);
+      Dialogs.showLongSnackBar(this, e.message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('ActivitiesPage'));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //
+            Text('Aktivnosti', style: Theme.of(context).textTheme.titleLarge),
+            ...data.map((e) {
+              return SizedBox(
+                width: double.infinity,
+                child: Card(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Naziv: ${e.name}', style: const TextStyle(fontWeight: FontWeight.bold)),
+                        Text('Tip: ${activityTypes.firstWhere((t) => t.key == e.type).name}'),
+                        Text('Početak: ${DateFormats.dateTime.format(e.startDateTime)}'),
+                        Text('Kraj: ${DateFormats.dateTime.format(e.endDateTime)}'),
+                        const Text('Bilješke:', style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text(e.notes),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+            //
+            const SizedBox(height: 20),
+
+            Text('Dodaj', style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 20),
+            TextField(
+              controller: tcName,
+              decoration: const InputDecoration(
+                isDense: true,
+                label: Text('Naziv'),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            DropdownMenu(
+              label: const Text('Tip'),
+              width: 200,
+              onSelected: (value) {
+                setState(() => activityType = value);
+              },
+              dropdownMenuEntries: activityTypes.map((e) {
+                return DropdownMenuEntry(
+                  value: e,
+                  label: e.name,
+                );
+              }).toList(),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text('Početak', style: Theme.of(context).textTheme.bodyLarge),
+            DateTimePicker(
+              value: startDateTime,
+              onUpdate: (value) {
+                setState(() => startDateTime = value);
+              },
+            ),
+            const SizedBox(height: 20), Text('Kraj', style: Theme.of(context).textTheme.bodyLarge),
+            DateTimePicker(
+              value: endDateTime,
+              onUpdate: (value) {
+                setState(() => endDateTime = value);
+              },
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: tcNotes,
+              minLines: 3,
+              maxLines: 8,
+              decoration: const InputDecoration(
+                isDense: true,
+                label: Text('Bilješke'),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            FilledButton(
+              onPressed: () => add(),
+              child: const Text('Dodaj'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    tcName.dispose();
+    tcNotes.dispose();
+    super.dispose();
   }
 }
