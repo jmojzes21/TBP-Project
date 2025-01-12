48b1c2a43f3584c4cef1a6e264804717502554b1
jmojzes21 <jmojzes21@student.foi.hr>
2025-01-12 15:41:44 +0100

    Set locale to hr

--- tbp_app/lib/main.dart
+++ tbp_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tbp_app/ui/pages/entry_page.dart';
 
 void main() {
@@ -12,6 +13,14 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('hr', 'HR'),
+      ],
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
       ),
